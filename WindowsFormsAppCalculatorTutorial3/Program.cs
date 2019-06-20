d4c1b722c31999b10d9aa689c35a948a9e251b12
songwut <songwut@enoactic.com>
2019-06-20 17:20:04 +0700

    commit main con

--- WindowsFormsAppCalculatorTutorial3/Program.cs
+++ WindowsFormsAppCalculatorTutorial3/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsAppCalculatorTutorial
         [STAThread]
         static void Main()
         {
+            //main con
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
