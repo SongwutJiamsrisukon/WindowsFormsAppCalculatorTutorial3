f58a9744339ce67860cb113b4376bb783239b429
songwut <songwut@enoactic.com>
2019-06-20 17:18:15 +0700

    comment conflict

--- WindowsFormsAppCalculatorTutorial3/Program.cs
+++ WindowsFormsAppCalculatorTutorial3/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsAppCalculatorTutorial
         [STAThread]
         static void Main()
         {
+            //conflict
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
