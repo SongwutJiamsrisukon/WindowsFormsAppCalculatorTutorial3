84d696cd877c98e09e44f58f45f39d83f4aefb6b
songwut <songwut@enoactic.com>
2019-06-21 17:45:01 +0700

    1

--- WindowsFormsAppCalculatorTutorial3/Program.cs
+++ WindowsFormsAppCalculatorTutorial3/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsAppCalculatorTutorial
         [STAThread]
         static void Main()
         {
-            //createCalculateNewBranch
+            //createCalculateNewBranch2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
